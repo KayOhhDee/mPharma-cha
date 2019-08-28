fbd359123693c02fc5a06407544b258d555f5d16
kod3180 <kod3180@gmail.com>
2019-08-28 15:42:16 +0000

    head text added

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "../redux";
 import NavBar from "./Navbar";
+import Main from "./Main";
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <NavBar />
+    <Main />
   </Provider>
 );
 
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <div className="head-text">
+          <p>No row selected</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main
