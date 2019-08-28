0ba38ba9f43efd726278c563ae648ab29b1375e8
kod3180 <kod3180@gmail.com>
2019-08-28 15:35:53 +0000

    Navbar added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "../redux";
+import NavBar from "./Navbar";
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
-
+    <NavBar />
   </Provider>
 );
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <div className="navbar">
+        <span className="navbar-brand">mPharma</span>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
