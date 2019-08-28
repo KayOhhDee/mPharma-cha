b5c4471199365df7a14fbe61e24d36e84ff7cb42
kod3180 <kod3180@gmail.com>
2019-08-28 15:53:28 +0000

    action buttons added and styled

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,6 +7,32 @@ class Main extends Component {
         <div className="head-text">
           <p>No row selected</p>
         </div>
+        <div className="button-container">
+          <div className="button-container-sub">
+            <button
+              style={{ backgroundColor: "#ffbb33" }}
+              className="button"
+            >
+              Add
+            </button>
+          </div>
+          <div className="button-container-sub">
+            <button
+              style={{ backgroundColor: "#ffbb33" }}
+              className="button"
+            >
+              Edit
+            </button>
+          </div>
+          <div className="button-container-sub">
+            <button
+              style={{ backgroundColor: "#FF3547" }}
+              className="button"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
